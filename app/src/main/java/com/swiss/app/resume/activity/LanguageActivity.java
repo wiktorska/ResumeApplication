13af354e7751a1e13825f31c3ea39b2fce10623e
wiktorska <aleksandra.wiktorska@yahoo.pl>
2014-10-03 12:01:55 +0200

    Initial commit

---
+++ app/src/main/java/com/swiss/app/resume/activity/LanguageActivity.java
@@ -0,0 +1,54 @@
+package com.swiss.app.resume.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import com.swiss.app.resume.R;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import java.util.Locale;
+
+
+@EActivity(R.layout.language_activity_main)
+public class LanguageActivity extends Activity {
+
+    private static final String TAG = LanguageActivity.class.getName();
+    @ViewById(R.id.de_label)
+    Button germanyLabel;
+
+    @ViewById(R.id.en_label)
+    Button englishLabel;
+
+    @ViewById(R.id.pl_label)
+    Button polishLabel;
+
+    Locale myLocale;
+
+   @Click({R.id.de_label,R.id.en_label,R.id.pl_label})
+    void chooseLanguage(View view) {
+
+        if (view.getId() == germanyLabel.getId()) {
+            setLocale("de");
+        } else if (view.getId() == englishLabel.getId()) {
+            setLocale("en");
+        } else if (view.getId() == polishLabel.getId()){
+            setLocale("pl");
+        }
+    }
+    public void setLocale(String lang) {
+        myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, LanguageActivity_.class);
+        startActivity(refresh);
+    }
+
+}
